66d4a77025e218952718d0a57d0aeac717ce14ee
Mohasina Afk <prantyafrin@gmail.com>
2020-08-24 08:20:50 -0700

    full hunt

---
+++ js/main.js
@@ -0,0 +1,289 @@
+
+
+
+
+
+//banner js @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+$('#banner-part').slick({
+//    dots: false,
+//    infinite: true,
+//    speed: 3000,
+//    slidesToShow: 1,
+//    slidesToScroll: 1,
+//    autoplay: true,
+//    autoplaySpeed: 3000,
+//    arrows: true,
+//    prevArrow: '<i class="fas fa-arrow-left prev"></i>',
+//    nextArrow: '<i class="fas fa-arrow-right next"></i>',
+//    fade: true,
+//    cssEase: 'linear',
+//    responsive: [
+//        {
+//            breakpoint: 1024,
+//            settings: {
+//                slidesToShow: 3,
+//                slidesToScroll: 3,
+//                infinite: true,
+//                dots: true
+//            }
+//    },
+//        {
+//            breakpoint: 600,
+//            settings: {
+//                slidesToShow: 2,
+//                slidesToScroll: 2
+//            }
+//    },
+//        {
+//            breakpoint: 480,
+//            settings: {
+//                slidesToShow: 1,
+//                slidesToScroll: 1
+//            }
+//    }
+//  ]
+//});
+
+
+
+
+// portfolio slick @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+$('.pslider').slick({
+    dots: false,
+    infinite: true,
+    speed: 3000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+//    arrows: true,
+//    prevArrow: '<i class="fas fa-arrow-left prev"></i>',
+//    nextArrow: '<i class="fas fa-arrow-right next"></i>',
+//    fade: true,
+//    cssEase: 'linear',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+    },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+    },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+    }
+  ]
+});
+
+
+
+
+
+//venubox js @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+$('.venobox').venobox({
+    border: '5px',
+});
+
+
+
+
+//service part js @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+$('.service-item').slick({
+    dots: false,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: true,
+    prevArrow: '<i class="fas fa-chevron-up up"></i>',
+    nextArrow: '<i class="fas fa-chevron-down down"></i>',
+    vertical: true,
+    verticalSwiping: true,
+    centerMode: true,
+    centerPadding: '0px',
+    focusOnSelect: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+    },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+    },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+    }
+  ]
+});
+
+
+
+
+
+//testimonial-img part js  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+$('.slider-img').slick({
+    dots: false,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: true,
+    prevArrow: '<i class="fas fa-chevron-up up"></i>',
+    nextArrow: '<i class="fas fa-chevron-down down"></i>',
+    vertical: true,
+    verticalSwiping: true,
+    centerMode: true,
+    centerPadding: '0px',
+    focusOnSelect: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+    },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+    },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+    }
+  ]
+});
+
+
+$('.testimonial-details').slick({
+    dots: false,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: false,
+    vertical: true,
+    verticalSwiping: true,
+//    centerMode: true,
+//    centerPadding: '0px',
+//    focusOnSelect: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+    },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+    },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+    }
+  ]
+});
+
+
+
+
+
+
+//counter part js@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+$('.counter').counterUp({
+                delay: 10,
+                time: 1000
+            });
+
+
+
+
+$('.next2-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 3000,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    centerMode: true,
+    centerPadding: '0px',
+    focusOnSelect: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+    },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+    },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+    }
+  ]
+});
+ 
+
+
+
+
+
